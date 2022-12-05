8a60877edf2ae1afc5b2371d071df9a8a8f7a404
Mikhailova <mkhlvzh@yandex.ru>
2022-12-05 23:26:32 +0500

    integration test update

--- src/test/java/ru/mikhailova/customerService/integration/AbstractIntegrationTest.java
+++ src/test/java/ru/mikhailova/customerService/integration/AbstractIntegrationTest.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,5 +35,13 @@ public abstract class AbstractIntegrationTest {
         return objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(), response);
     }
 
+    protected <T> T performStart(Object requestBody, Class<T> response) throws Exception {
+        ResultActions resultActions = mockMvc.perform(post(API_URL + "/start")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isOk());
+        return objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(), response);
+    }
 
 }
--- src/test/java/ru/mikhailova/customerService/integration/IntegrationTest.java
+++ src/test/java/ru/mikhailova/customerService/integration/IntegrationTest.java
@@ -1,20 +1,54 @@
 package ru.mikhailova.customerService.integration;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.mikhailova.customerService.controller.dto.ClaimDto;
+import ru.mikhailova.customerService.controller.dto.ClaimStartRequestDto;
+import ru.mikhailova.customerService.repository.ClaimRepository;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class IntegrationTest extends AbstractIntegrationTest {
-    private TypeReference<List<ClaimDto>> typeReference = new TypeReference<List<ClaimDto>>(){};
+    @Autowired
+    private ClaimRepository claimRepository;
+    private TypeReference<List<ClaimDto>> typeReference = new TypeReference<List<ClaimDto>>() {
+    };
+
+    @BeforeEach
+    void setUp() throws Exception {
+        ClaimStartRequestDto dto = new ClaimStartRequestDto();
+        dto.setClaimType("OFFER");
+        dto.setCustomerContactInfo("+147896325");
+        dto.setDescription("Application upgrade");
+
+        String start = performStart(dto, String.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        claimRepository.deleteAll();
+    }
+
+    @Test
+    void couldStartSupport() {
+
+    }
 
     @Test
     void couldGetAllClaims() throws Exception {
         List<ClaimDto> claims = performGetAll("/get-all", typeReference);
 
         assertThat(claims).isNull();
+    }
+
+    @Test
+    void couldGetClaimById() {
+
+
     }
 }
